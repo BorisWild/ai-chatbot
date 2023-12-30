bfb10876d91f9c0b91a01ef117647c155b5f6a92
boris <boriswild@gmail.com>
2023-12-31 01:46:34 +0300

    fix

--- components/chat.tsx
+++ components/chat.tsx
@@ -52,11 +52,11 @@ export function Chat({id, initialMessages, className}: ChatProps) {
                 }
             },
             onFinish() {
-                if (!path.includes('chat')) {
-                    //router.push(`/chat/${id}`, { shallow: true, scroll: false })
-                    router.push(`/chat/${id}`, {scroll: false})
-                    router.refresh()
-                }
+                // if (!path.includes('chat')) {
+                //     //router.push(`/chat/${id}`, { shallow: true, scroll: false })
+                //     router.push(`/chat/${id}`, {scroll: false})
+                //     router.refresh()
+                // }
             }
         })
     return (
@@ -82,41 +82,7 @@ export function Chat({id, initialMessages, className}: ChatProps) {
                 setInput={setInput}
             />
 
-            <Dialog open={previewTokenDialog} onOpenChange={setPreviewTokenDialog}>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Enter your OpenAI Key</DialogTitle>
-                        <DialogDescription>
-                            If you have not obtained your OpenAI API key, you can do so by{' '}
-                            <a
-                                href="https://platform.openai.com/signup/"
-                                className="underline"
-                            >
-                                signing up
-                            </a>{' '}
-                            on the OpenAI website. This is only necessary for preview
-                            environments so that the open source community can test the app.
-                            The token will be saved to your browser&apos;s local storage under
-                            the name <code className="font-mono">ai-token</code>.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <Input
-                        value={previewTokenInput}
-                        placeholder="OpenAI API key"
-                        onChange={e => setPreviewTokenInput(e.target.value)}
-                    />
-                    <DialogFooter className="items-center">
-                        <Button
-                            onClick={() => {
-                                setPreviewToken(previewTokenInput)
-                                setPreviewTokenDialog(false)
-                            }}
-                        >
-                            Save Token
-                        </Button>
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
+           
         </>
     )
 }
